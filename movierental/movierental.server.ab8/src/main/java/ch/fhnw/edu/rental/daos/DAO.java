b26115a44b36d4bea9dbd620046b10e1cc86e7dc
Pascal Schwarz <iso@thinktux.net>
2013-11-04 10:44:03 +0100

    transactional und txMan

--- movierental/movierental.server.ab8/src/main/java/ch/fhnw/edu/rental/daos/DAO.java
+++ movierental/movierental.server.ab8/src/main/java/ch/fhnw/edu/rental/daos/DAO.java
@@ -2,6 +2,9 @@ package ch.fhnw.edu.rental.daos;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
 public interface DAO<T> {
 	T getById(Long id);
 	List<T> getAll();
